7dfd3db9edf59d02d4fb50ed99e36087d1d61b91
Dtesch9 <alucard.s8123@gmail.com>
2025-12-23 12:49:02 -0300

    update

--- lua/tiny-code-action/pickers/buffer_utils/window.lua
+++ lua/tiny-code-action/pickers/buffer_utils/window.lua
@@ -22,7 +22,6 @@ local function calculate_window_position(lines, config)
   if parent_win_coords then
     return width, height, parent_win_coords.row, parent_win_coords.col
   end
-
   local row, col, win_row, win_col, shift
   local position = config.picker and config.picker.opts and config.picker.opts.position or "cursor"
 
@@ -30,8 +29,14 @@ local function calculate_window_position(lines, config)
     local nvim_width = vim.o.columns
     local nvim_height = vim.o.lines
 
-    row = math.floor(nvim_height / 2) + math.floor(height / 2)
-    col = math.floor((nvim_width - width) / 2)
+    local inner_width = width
+    local inner_height = height
+
+    local full_width = inner_width + 2
+    local full_height = inner_height + 2
+
+    row = math.floor((nvim_height - full_height) / 2)
+    col = math.floor((nvim_width - full_width) / 2)
   else
     win_row, win_col = unpack(vim.api.nvim_win_get_position(0))
     row = vim.fn.winline()
